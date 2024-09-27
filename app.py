2c9c3e50aab879c508ff19998b483550e7ab6e1a
andrewcchan <40149441+andrewcchan@users.noreply.github.com>
2024-09-27 09:41:26 -0700

    post request

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
 st.write("Hello, World!")
+
+import requests
+
+url = "https://models.inference.ai.azure.com/chat/completions"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + st.secrets["GITHUB_MODEL_API"]
+}
+data = {
+    "messages": [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the capital of France?"},
+    ],
+    "model": "Phi-3-medium-128k-instruct"
+}
+
+response = requests.post(url, headers=headers, json=data)
+st.write(response.json())
+
